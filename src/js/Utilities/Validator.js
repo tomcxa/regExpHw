fe878cb27726f8a65ed41072828ba941538080ed
Anton Golikov <tomcxa@yandex.ru>
2020-03-06 10:42:06 +0300

    init commit

--- src/js/Utilities/Validator.js
+++ src/js/Utilities/Validator.js
@@ -1,11 +1,11 @@
 /* eslint-disable class-methods-use-this */
 export default class Validator {
     validateUsername(username) {
-        const charStart = !!(username.match(/^[a-z]/gi)); // начинается на букву - true
-        const charEnd = !!(username.match(/[a-z]$/gi)); // кончается на букву - true
-        const notThreeNumInRow = !(username.match(/\d{4,}/g)); // нет более 3 символов подряд - true
-        const validSymbols = !(username.match(/[^\w-]/g)); // латинские, цифры, _, -
-        const isCorrect = charStart && charEnd && notThreeNumInRow && validSymbols;
+        const charStart = (/^[a-z]/gi.test(username)); // начинается на букву - true
+        const charEnd = (/[a-z]$/gi.test(username)); // кончается на букву - true
+        const threeNumInRow = (/\d{4,}/g.test(username)); // нет более 3 символов подряд - true
+        const invalidSymbols = (/[^\w-]/g.test(username)); // латинские, цифры, _, -
+        const isCorrect = charStart && charEnd && !threeNumInRow && !invalidSymbols;
 
         if (!isCorrect) {
             throw new Error('Введите корректный нэйм');
